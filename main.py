316c2abc867a6a00478351f226ab0a212480b99a
Lay4u <kmr345@naver.com>
2018-11-16 09:26:54 +0900

    Last Accuracy: 0.930375%     Valid error: 0.63189967565611
            nDropout = 0.5
            model.add(Flatten())
            model.add(Dense(128, activation='relu', kernel_initializer='glorot_uniform'))
            model.add(Dropout(nDropout))
            model.add(Dense(516, activation='relu', kernel_initializer='glorot_uniform'))
            model.add(Dropout(nDropout))
            model.add(Dense(len(self.target_class)))
            model.add(Activation('softmax'))

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:255112972db2197ed6384b5281f7ba9b9d9c3a7935702932fb6a3e49321c516c
-size 12634
+oid sha256:e295413e37ecc94dae4804b2bd30c00269b54e195f05564cfdeac81deed74625
+size 14096
